51069e1ec47d1fb4116a65069b2152da88510b95
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2023-12-26 18:17:59 -0500

    terminado

--- FT-M1-master/08-Algoritmos-II/homework/homework.js
+++ FT-M1-master/08-Algoritmos-II/homework/homework.js
@@ -6,8 +6,25 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const pivot = array[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < pivot) {
+        left.push(array[i]);
+      } else {
+        right.push(array[i]);
+      }
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
 
-}
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
@@ -16,7 +33,37 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
-}
+
+    if (array.length <= 1) {
+      return array; // Ya está ordenado si es de longitud 0 o 1
+    }
+  
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+  
+    // Agregar elementos restantes, si los hay
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+  }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
